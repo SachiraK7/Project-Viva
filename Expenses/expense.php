db774ae6dc7ef60c24aad16bcc22c67540a718bc
SachiraK7 <sachirakavindu7@gmail.com>
2026-02-22 13:45:09 +0530

    drop down box update

--- Expenses/expense.php
+++ Expenses/expense.php
@@ -187,7 +187,7 @@ if ($db_connected) {
                 </table>
             </div>
 
-            <div class="expense-form">
+            <div class="expense-form" style="margin-bottom: 150px;">
                 <h2>Add New Expense</h2>
                 <form id="addExpenseForm">
                     <input type="hidden" name="action" value="add_expense">
@@ -198,7 +198,6 @@ if ($db_connected) {
                     
                     <div class="form-group">
                         <label>Category</label>
-<<<<<<< HEAD
                         <select name="category" id="categorySelect" required>
                             <option value="" disabled selected>Choose Category</option>
                             <option value="Bills">Bills</option>
@@ -212,19 +211,6 @@ if ($db_connected) {
                             <option value="Transport">Transport</option>
                             <option value="Other">Other</option>
                         </select>
-=======
-                        <div class="cat-group">
-                            <select name="category" id="categorySelect" style="width: 100%; box-sizing: border-box;">
-                                <option value="" disabled selected>Choose Category</option>
-                                <option value="Bills">Bills</option>
-                                <option value="Education">Education</option>
-                                <option value="Entertainment">Entertainment</option>
-                                <option value="Food">Food</option>
-                                <option value="Transport">Transport</option>
-                                <option value="Other">Other</option>
-                            </select>
-                        </div>
->>>>>>> 9d039237b52eaebadf2a1cc39aa23d9aae5f75c8
                     </div>
                     
                     <div class="form-group">
